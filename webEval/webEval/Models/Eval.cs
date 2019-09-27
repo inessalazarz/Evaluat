3b70d5bb55d94a82d31d72b757c940ec0bc915aa
Ines Salazar Zambrana <Ines.Salazar.Zambrana-1@ou.edu>
2019-09-27 01:44:01 -0400

    Solucion completa

---
+++ webEval/webEval/Models/Eval.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace webEval.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Eval
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+
+        public string Sex { get; set; }
+
+
+
+    }
+}
